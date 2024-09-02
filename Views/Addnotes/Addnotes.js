9578e2690c61e5b8cbc7a7321a46af52f58c4020
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-02 20:26:02 +0530

    import css file for each component

---
+++ Views/Addnotes/Addnotes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Addnotes.css'
+function Addnotes() {
+  return (
+    <div>Addnotes</div>
+  )
+}
+
+export default Addnotes
---
+++ Views/Shownotes/Shownotes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Shownotes.css'
+
+function Shownotes() {
+  return (
+    <div>Shownotes</div>
+  )
+}
+
+export default Shownotes
