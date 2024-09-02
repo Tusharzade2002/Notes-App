114e0632687944e46fcff0a4e7b78d44875dadb1
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-02 20:12:59 +0530

    Routing Apply

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import { createBrowserRouter , RouterProvider} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-  <h1>
-    Notes App
-  </h1>
 
-);
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<h1>Home</h1>,
+  },
+  {
+    path:"/addnotes",
+    element:<h1>addnotes</h1>,
+  },
+  {
+    path:"/shownotes",
+    element:<h1>shownotes</h1>
+  }
+])
 
-
+root.render(<RouterProvider router={router} />)
