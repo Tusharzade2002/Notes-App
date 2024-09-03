2f3ca04797eb25fef8e6c7fe78ce03a617a15d32
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-03 23:02:36 +0530

    feature card added

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import './../../index.css'
+import shield from './shield.png'
 function Home() {
   return (
     <div>
@@ -9,6 +10,24 @@ function Home() {
         Your Packet Friendly Notes Taking App. <del>Never Forget Anything Again!</del> 
         <span className='App-description-highlight'> Remember Everything! 🧠</span>
       </p>
+
+      <div>
+        <h2 className='text-centre'>Why You Should Use Quick Notes</h2>
+        <div className='cards-container'>
+        <div className='feature-card '>
+          <img className='shield-img' src={shield} alt='shield image'/>
+         <p className='feature-text'>Notes Will be Saved Securely in Your Browser's Local Storage</p>
+        </div>
+        <div className='feature-card '>
+          <img className='shield-img' src={shield} alt='shield image'/>
+         <p className='feature-text'>Notes Will be Saved Securely in Your Browser's Local Storage</p>
+        </div>
+        <div className='feature-card '>
+          <img className='shield-img' src={shield} alt='shield image'/>
+         <p className='feature-text'>Notes Will be Saved Securely in Your Browser's Local Storage</p>
+        </div>
+        </div>
+      </div>
 
       <div className='buttons-container'>
         <button className='btn primary-btn'>Add Notes</button>
