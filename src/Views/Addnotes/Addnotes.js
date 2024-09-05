36f964691b99e27a7485b1a358eac2f1b785af07
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-05 22:41:28 +0530

    input box and clear button added

--- src/Views/Addnotes/Addnotes.js
+++ src/Views/Addnotes/Addnotes.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Addnotes.css'
 import Homebutton from '../../Component/Homebutton/Homebutton'
 
 function Addnotes() {
-  
+  const [title , setTitle] =useState("");
   return (
     <div>
       <h2 className='text-centre primary-color App-name'>✍️Addnotes</h2>
       <Homebutton/> 
+      <input type='text' 
+      placeholder='enter the note'
+      value={title}
+      onChange={(e)=>
+       setTitle(e.target.value)}
+      />
+      
+      <button type='button' onClick={()=>{setTitle("")}}>
+                 Clear 
+      </button>
     </div>
   )
 }
