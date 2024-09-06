200204bf41bd6bc6a37ac00664accd0136069f59
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-06 22:31:47 +0530

    add  input box for description and category

--- src/Views/Addnotes/Addnotes.js
+++ src/Views/Addnotes/Addnotes.js
@@ -4,6 +4,8 @@ import Homebutton from '../../Component/Homebutton/Homebutton'
 
 function Addnotes() {
   const [title , setTitle] =useState("");
+  const[description,setDescription] =useState("")
+  const [category,setCategory] =useState("")
   return (
     <div>
       <h2 className='text-centre primary-color App-name'>✍️Addnotes</h2>
@@ -18,6 +20,21 @@ function Addnotes() {
       <button type='button' onClick={()=>{setTitle("")}}>
                  Clear 
       </button>
+      <input type='text' 
+      placeholder='enter the note'
+      value={description}
+      onChange={(e)=>
+       setDescription(e.target.value)}
+      />
+       <input type='text' 
+      placeholder='enter the note'
+      value={category}
+      onChange={(e)=>
+       setCategory(e.target.value)}
+      />
+
+
+
     </div>
   )
 }
