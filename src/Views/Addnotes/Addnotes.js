ec38545c69f2d90a952d2dec1e379e8e68db3e7c
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-07 22:51:51 +0530

    degin input boxes

--- src/Views/Addnotes/Addnotes.js
+++ src/Views/Addnotes/Addnotes.js
@@ -1,40 +1,57 @@
 import React, { useState } from 'react'
 import './Addnotes.css'
 import Homebutton from '../../Component/Homebutton/Homebutton'
+import EmojiPicker from 'emoji-picker-react';
 
 function Addnotes() {
   const [title , setTitle] =useState("");
   const[description,setDescription] =useState("")
   const [category,setCategory] =useState("")
+  const [emoji , setEmoji] =useState("")
+  const [EmojiDialog ,setEmojiDialog]=useState(false)
   return (
     <div>
       <h2 className='text-centre primary-color App-name'>✍️Addnotes</h2>
       <Homebutton/> 
       <input type='text' 
-      placeholder='enter the note'
+      placeholder='Title'
       value={title}
       onChange={(e)=>
        setTitle(e.target.value)}
+       className='user-input'
       />
-      
-      <button type='button' onClick={()=>{setTitle("")}}>
-                 Clear 
-      </button>
+ 
       <input type='text' 
-      placeholder='enter the note'
+      placeholder='description'
       value={description}
       onChange={(e)=>
        setDescription(e.target.value)}
+       className='user-input'
       />
-       <input type='text' 
-      placeholder='enter the note'
-      value={category}
-      onChange={(e)=>
-       setCategory(e.target.value)}
-      />
-
-
-
+   <select value={category} 
+   placeholder="category"
+   onClick={(e)=>{setCategory(e.target.value)}}>
+   <option value="personal">Personal</option>
+   <option value="working">Working</option>
+   <option value="shopping">Shopping</option>
+   <option value="learning">Learning</option>
+   </select>
+{
+  emoji
+}
+    <button type='button' onClick={()=>setEmojiDialog(true)}>
+    Emoji
+   </button>
+      <EmojiPicker open={EmojiDialog} 
+            searchDisabled={true} 
+            height="300px" 
+            width="400px" 
+            skinTonesDisabled={true}
+            onEmojiClick={(emojiobject)=>{
+            setEmoji(emojiobject.emoji)
+            setEmojiDialog(false)
+            }}
+      />  
     </div>
   )
 }
