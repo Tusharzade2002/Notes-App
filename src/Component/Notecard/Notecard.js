12825288b8a44796e558d9e9951396bd4d48f18f
Tusharzade <tusharzade29@gmail.com>
2024-09-26 20:11:31 +0530

    adding design

--- src/Component/Notecard/Notecard.js
+++ src/Component/Notecard/Notecard.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import './Notecard.css'
+import deleteicon from './delete.png';
+import { Simulate } from 'react-dom/test-utils';
+function Notescategory({category}){
+     const CATEGORY_EMOJIS ={
+        "shopping":"🛒",
+        "personal":"🧑‍🦰",
+        "working":"💼",
+        "health":"🏋️",
+        "learning":"📖"
+     }
+
+     return(
+      <div className={`shownote-category bg-${category}`}>
+           <span className='shownote-category-emoji'>{CATEGORY_EMOJIS[category]}</span>
+               {category[0].toUpperCase()+ category.slice(1)}
+      </div>
+     );
+}
+
+
 function Notecard({title,description,category,emoji}) {
   return (
     <div className='main-container'>
@@ -10,9 +30,12 @@ function Notecard({title,description,category,emoji}) {
 
 
            <div className='shownotes-text-container'>
-             <h2 className='shownotes-title'>{title}</h2>
-             <p className='shownotes-description'>{description}</p>
-             <span className='shownotes-category'>{category}</span>
+             <h2 className='shownote-title'>{title}</h2>
+             <p className='shownote-description'>{description}</p>
+             <span >
+             <Notescategory category={category} />
+             </span>
+             <img className='notecard-deletebutton' src={deleteicon} />
            </div>
     </div>
   )
